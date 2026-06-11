a8e3673f022fc7f49294a3d77cd5598369fc2072
Josephine Mundt <josephinemundt1297@gmail.com>
2026-06-11 20:55:03 +0200

    add: implement Sidebar component with navigation links

--- src/components/organism/sidebar.org.tsx
+++ src/components/organism/sidebar.org.tsx
@@ -1,3 +1,13 @@
+import { Link } from "@tanstack/react-router";
+
 export default function Sidebar() {
-  return <></>;
+  return (
+    <>
+      <nav>
+        <Link to="/">Dashboard</Link>
+        <Link to="/todo">To-Do</Link>
+        <Link to="/categories">Kategorien</Link>
+      </nav>
+    </>
+  );
 }
