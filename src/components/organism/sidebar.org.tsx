d381d74f736925fe2d32901851c8acde8590acac
Josephine Mundt <josephinemundt1297@gmail.com>
2026-06-11 21:41:12 +0200

    refactor: add comments to clarify link functionality in Sidebar component

--- src/components/organism/sidebar.org.tsx
+++ src/components/organism/sidebar.org.tsx
@@ -7,6 +7,10 @@ export default function Sidebar() {
         <Link to="/">Dashboard</Link>
         <Link to="/todo">To-Do</Link>
         <Link to="/categories">Kategorien</Link>
+        {/*Link to ist damit nur innerhalb der Seite eine einzelne
+        Komponente gerändert wird und nicht die gesammte Seite.
+        Wenn ich auf eine externe Seite verlinken möchte kann ich 
+        wie gewohnt <a href=""> nutzen*/}
       </nav>
     </>
   );
